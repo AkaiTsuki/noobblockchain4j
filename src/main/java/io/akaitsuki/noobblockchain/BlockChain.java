c625c111f1383cddb138f01432c1ba5021e45da9
AkaiTsuki <liu.jiach@husky.neu.edu>
2018-03-06 22:02:10 +0800

    implement noob blockchain with block

---
+++ src/main/java/io/akaitsuki/noobblockchain/BlockChain.java
@@ -0,0 +1,42 @@
+package io.akaitsuki.noobblockchain;
+
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jiachiliu on 3/6/18.
+ */
+public class BlockChain {
+    private List<Block> chain = new ArrayList<Block>();
+
+    public void addBlock(Block block) {
+        chain.add(block);
+    }
+
+    public boolean isChainValid() {
+        Block prev, cur;
+
+        for(int i=1; i<chain.size(); i++) {
+            prev = chain.get(i-1);
+            cur = chain.get(i);
+
+            // current block get modified
+            if(!cur.getHash().equals(cur.calculateHash())) {
+                return false;
+            }
+
+            // previous hash in current block is not same as registered hash in previous block
+            if(!cur.getPreviousHash().equals(prev.getHash())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public String toGsonString() {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(chain);
+    }
+}
--- src/main/java/io/akaitsuki/noobblockchain/Main.java
+++ src/main/java/io/akaitsuki/noobblockchain/Main.java
@@ -6,12 +6,21 @@ package io.akaitsuki.noobblockchain;
 public class Main {
     public static void main(String[] args) {
         Block b1 = new Block("first block is genesis block", "0");
-        System.out.println(b1);
-
         Block b2 = new Block("second block", b1.getHash());
-        System.out.println(b2);
-
         Block b3 = new Block("third block", b2.getHash());
-        System.out.println(b3);
+
+        BlockChain blockChain = new BlockChain();
+        blockChain.addBlock(b1);
+        blockChain.addBlock(b2);
+        blockChain.addBlock(b3);
+
+        System.out.println(blockChain.toGsonString());
+
+        // print true
+        System.out.println(blockChain.isChainValid());
+
+        // modify existing block will cause validation fail
+        b2.setData("new data");
+        System.out.println(blockChain.isChainValid());
     }
 }
